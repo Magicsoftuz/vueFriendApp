1daa461b0bd3d4e78a144f6f3436b03178da931c
MagicSoft <magicsoft.uz@gmail.com>
2022-07-30 13:04:16 +0500

    Vue Friend App

---
+++ vue-cli-01-a-new-vue-project 2/src/App.vue
@@ -0,0 +1,122 @@
+<template>
+  <section>
+    <ul v-for="(friendObj, index) in friends" :key="index">
+      <Friend
+        :friends="friendObj"
+        :best="bestFriend"
+        :index="index"
+        :info="getInfo"
+      />
+    </ul>
+  </section>
+</template>
+
+<script>
+import Friend from "./components/Friend.vue";
+
+export default {
+  data() {
+    return {
+      bestFriend: false,
+      information: {},
+      friends: [
+        {
+          name: "Manuel Lorenz",
+          phone: "01234 5678 991",
+          email: "manuel@localhost.com",
+        },
+        {
+          name: "Umid Lorenz",
+          phone: "01234 23232 991",
+          email: "fdsjkh@localhost.com",
+        },
+        {
+          name: "Jafar Lorenz",
+          phone: "3434 23232 991",
+          email: "kljlkjlk@localhost.com",
+        },
+        {
+          name: "Mironshoh Lorenz",
+          phone: "8687 23232 991",
+          email: "kljkljh@localhost.com",
+        },
+      ],
+    };
+  },
+  components: {
+    Friend,
+  },
+  methods: {
+    getInfo(info) {
+      this.information = info;
+      console.log(this.information);
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: "Jost", sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: #58004d;
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #58004d;
+  margin: 0 0 1rem 0;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid #ff0077;
+  background-color: #ff0077;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+</style>
---
+++ vue-cli-01-a-new-vue-project 2/src/components/Friend.vue
@@ -0,0 +1,37 @@
+<template>
+  <li>
+    <h2>{{ friends.name }} {{ changeFriend ? "Best Friend" : "" }}</h2>
+    <button @click="changeToggle">Show Details</button>
+    <button @click="clickBtn(friends)">Best Friend</button>
+    <ul v-if="toggleInfo">
+      <li><strong>Phone:</strong> {{ friends.phone }}</li>
+      <li><strong>Email:</strong> {{ friends.email }}</li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+  props: ["friends", "best", "index", "info"],
+  data() {
+    return {
+      message: "Hello Component",
+      toggleInfo: true,
+      changeFriend: this.best,
+    };
+  },
+  methods: {
+    changeToggle() {
+      this.toggleInfo = !this.toggleInfo;
+    },
+    changeBest() {
+      this.changeFriend = !this.changeFriend;
+    },
+    clickBtn(friends) {
+      this.info(friends);
+    },
+  },
+};
+</script>
+
+<style></style>
